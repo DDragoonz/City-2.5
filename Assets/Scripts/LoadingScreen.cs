8a27d6859637cfa5de0e6b399f3c713c69276862
rizkyade <dragoonz.d@gmail.com>
2016-08-09 06:32:19 +0700

    Another morning update
    posisi UI, loading screen dan select town

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (loadRegion ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator loadRegion(){
+		yield return new WaitForSeconds (3f);
+		
+		AsyncOperation async = SceneManager.LoadSceneAsync ("Town "+GameManager.activeTown.id);
+		
+		while (!async.isDone) {
+			yield return null;
+		}
+	}
+}
--- Assets/Scripts/UI_Manager_World.cs
+++ Assets/Scripts/UI_Manager_World.cs
@@ -2,12 +2,14 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
 
 public class UI_Manager_World : MonoBehaviour {
 
 	private RotateGlobe rg;
-	public Text townText,deskripsi;
-	public GameObject panel1, panel2,introText;
+	public Text townText,deskripsi,money,happiness,education,health,polution,crime,traffic,lastPlayed;
+	public GameObject[] preSelectionObjects;
+	public GameObject[] townSelectionObjects;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +17,19 @@ public class UI_Manager_World : MonoBehaviour {
 		rg = FindObjectOfType<RotateGlobe> ();
 		townText.text = GameManager.activeTown.name;
 		deskripsi.text = GameManager.activeTown.description;
+		if (GameManager.activeTown.hasPlayed) {
+			health.text = GameManager.activeTown.health.ToString ("F2");
+			happiness.text = GameManager.activeTown.happiness.ToString ("F2");
+			education.text = GameManager.activeTown.education.ToString ("F2");
+			traffic.text = GameManager.activeTown.traffic.ToString ("F2");
+			crime.text = GameManager.activeTown.crime.ToString ("F2");
+			polution.text = GameManager.activeTown.polution.ToString ("F2");
+			if (PlayerPrefs.HasKey ("time")) {
+				long temp = Convert.ToInt64 (PlayerPrefs.GetString ("time"));
+				DateTime old = DateTime.FromBinary (temp);
+				lastPlayed.text = "Last Played : " + old.ToString();
+			}
+		}
 
 	}
 	
@@ -32,6 +47,15 @@ public class UI_Manager_World : MonoBehaviour {
 		GameManager.activeTown = GameManager.Towns.Find (x => x.id == rg.currentIndex);
 		townText.text = GameManager.activeTown.name;
 		deskripsi.text = GameManager.activeTown.description;
+		money.text = GameManager.activeTown.money.ToString("F2");
+		if (GameManager.activeTown.hasPlayed) {
+			health.text = GameManager.activeTown.health.ToString ("F2");
+			happiness.text = GameManager.activeTown.happiness.ToString ("F2");
+			education.text = GameManager.activeTown.education.ToString ("F2");
+			traffic.text = GameManager.activeTown.traffic.ToString ("F2");
+			crime.text = GameManager.activeTown.crime.ToString ("F2");
+			polution.text = GameManager.activeTown.polution.ToString ("F2");
+		}
 	}
 
 	public void prevTown(){
@@ -39,28 +63,33 @@ public class UI_Manager_World : MonoBehaviour {
 		GameManager.activeTown = GameManager.Towns.Find (x => x.id == rg.currentIndex);
 		townText.text = GameManager.activeTown.name;
 		deskripsi.text = GameManager.activeTown.description;
+		if (GameManager.activeTown.hasPlayed) {
+			money.text = GameManager.activeTown.money.ToString ("F2");
+			health.text = GameManager.activeTown.health.ToString ("F2");
+			happiness.text = GameManager.activeTown.happiness.ToString ("F2");
+			education.text = GameManager.activeTown.education.ToString ("F2");
+			traffic.text = GameManager.activeTown.traffic.ToString ("F2");
+			crime.text = GameManager.activeTown.crime.ToString ("F2");
+			polution.text = GameManager.activeTown.polution.ToString ("F2");
+		}
 	}
 
 	public void selectTownActive(){
-		panel1.SetActive (true);
-		panel2.SetActive (true);
-		introText.SetActive (false);
+		foreach (GameObject g in preSelectionObjects) {
+			g.SetActive(false);
+		}
+		foreach (GameObject g in townSelectionObjects) {
+			g.SetActive(true);
+		}
+
 		GameManager.state = State.SELECT_TOWN;
 	}
 
 	public void loadRegionScene(){
 //		GameManager.activeTown = GameManager.userdata.towns [rg.currentIndex];
-
-		StartCoroutine (loadRegion ());
+		SceneManager.LoadSceneAsync ("Loading Screen Town");
+//		StartCoroutine (loadRegion ());
 	}
 
-	IEnumerator loadRegion(){
-		yield return new WaitForSeconds (.1f);
 
-		AsyncOperation async = SceneManager.LoadSceneAsync ("Town "+rg.currentIndex);
-
-		while (!async.isDone) {
-			yield return null;
-		}
-	}
 }
